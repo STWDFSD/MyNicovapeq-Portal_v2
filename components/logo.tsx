dde2905fdd3e9af23a74622cdf0865b1e623152c
STWDFSD <spuerblackgold@gmail.com>
2025-06-15 20:33:42 -0400

    fix deploy error

--- components/logo.tsx
+++ components/logo.tsx
@@ -9,7 +9,7 @@ export default function Logo({ className = "", textOnly = false }: LogoProps) {
   if (textOnly) {
     return (
       <div className={`${className} relative flex items-center justify-center`}>
-        <Image src="/images/new-logo.png" alt="mynicovapeQ logo" width={186} height={25} className="h-auto w-full" />
+        <Image src="/images/new-logo.png" alt="mynicovapeQ logo" width={186} height={25} className="h-auto" />
       </div>
     )
   }
